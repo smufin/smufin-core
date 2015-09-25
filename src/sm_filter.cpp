45f947543ac5df1cf6b3d1bf9e213c1cdf48ee91
Jordà Polo <jorda.polo@bsc.es>
2015-09-25 11:20:43 +0200

    Prepare filter for ECONT support

--- src/sm_filter.cpp
+++ src/sm_filter.cpp
@@ -306,9 +306,14 @@ void filter_sub_fstlst_normal(int pid, int fid, kseq_t *seq, const char *sub,
     for (int i = 0; i <= len - KMER_LEN; i++) {
         strncpy(kmer, &sub[i], KMER_LEN);
         kmer[KMER_LEN] = '\0';
-
         for (int j = 0; j < 4; j++) {
             kmer[0] = vars[j];
+
+            uint32_t normal_parent = 0;
+            uint32_t tumour_parent = 0;
+            uint32_t normal_counts[4] = {0};
+            uint32_t tumour_counts[4] = {0};
+
             for (int k = 0; k < 4; k++) {
                 kmer[KMER_LEN - 1] = vars[k];
 
@@ -327,14 +332,26 @@ void filter_sub_fstlst_normal(int pid, int fid, kseq_t *seq, const char *sub,
 
                 uint32_t cnr = it->second.first;
                 uint32_t ctr = it->second.second;
-                char buf[256] = {0};
+
+                normal_parent += cnr;
+                tumour_parent += ctr;
+                normal_counts[k] = cnr;
+                tumour_counts[k] = ctr;
+            }
+
+            for (int k = 0; k < 4; k++) {
+                kmer[KMER_LEN - 1] = vars[k];
+
+                uint32_t cnr = normal_counts[k];
+                uint32_t ctr = tumour_counts[k];
+
                 if (ctr >= 4 && cnr == 0) {
+                    char buf[256] = {0};
                     sprintf(buf, "@%s\n%s\n+\n%s",
                             seq->name.s, seq->seq.s, seq->qual.s);
                     filter_nn_mutex.lock();
                     filter_nn_reads.insert(buf);
                     filter_nn_mutex.unlock();
-                    return;
                 }
             }
         }
@@ -350,33 +367,53 @@ void filter_sub_fstlst_cancer(int pid, int fid, kseq_t *seq, const char *sub,
     const char vars[] = "ACGT";
 
     char kmer[KMER_LEN + 1];
+
     for (int i = 0; i <= len - KMER_LEN; i++) {
         strncpy(kmer, &sub[i], KMER_LEN);
         kmer[KMER_LEN] = '\0';
 
-        uint32_t m = 0;
-        memcpy(&m, &kmer[0], MAP_LEN);
-        hash_4c_map(m);
+        char last = kmer[KMER_LEN - 1];
+        uint32_t normal_parent = 0;
+        uint32_t tumour_parent = 0;
+        uint32_t normal_counts[4] = {0};
+        uint32_t tumour_counts[4] = {0};
 
-        if (map_l1[m] != pid)
-            continue;
-        int sid = map_l2[m];
-        sm_key key = strtob4(kmer);
+        for (int k = 0; k < 4; k++) {
+            kmer[KMER_LEN - 1] = vars[k];
 
-        sm_table::const_iterator it = tables[sid].find(key);
-        if (it == tables[sid].end())
-            continue;
+            uint32_t m = 0;
+            memcpy(&m, &kmer[0], MAP_LEN);
+            hash_4c_map(m);
 
-        uint32_t cnr = it->second.first;
-        uint32_t ctr = it->second.second;
-        char buf[256] = {0};
+            if (map_l1[m] != pid)
+                continue;
+            int sid = map_l2[m];
+            sm_key key = strtob4(kmer);
+
+            sm_table::const_iterator it = tables[sid].find(key);
+            if (it == tables[sid].end())
+                continue;
+
+            uint32_t cnr = it->second.first;
+            uint32_t ctr = it->second.second;
+
+            normal_parent += cnr;
+            tumour_parent += ctr;
+            normal_counts[k] = cnr;
+            tumour_counts[k] = ctr;
+        }
+
+        kmer[KMER_LEN - 1] = last;
+        uint32_t cnr = normal_counts[codes[last] - '0'];
+        uint32_t ctr = tumour_counts[codes[last] - '0'];
+
         if (ctr >= 4 && cnr == 0) {
+            char buf[256] = {0};
             sprintf(buf, "@%s\n%s\n+\n%s",
                     seq->name.s, seq->seq.s, seq->qual.s);
             filter_tm_mutex.lock();
             filter_tm_reads.insert(buf);
             filter_tm_mutex.unlock();
-            return;
         }
     }
 
@@ -385,6 +422,12 @@ void filter_sub_fstlst_cancer(int pid, int fid, kseq_t *seq, const char *sub,
         kmer[KMER_LEN] = '\0';
         for (int j = 0; j < 4; j++) {
             kmer[0] = vars[j];
+
+            uint32_t normal_parent = 0;
+            uint32_t tumour_parent = 0;
+            uint32_t normal_counts[4] = {0};
+            uint32_t tumour_counts[4] = {0};
+
             for (int k = 0; k < 4; k++) {
                 kmer[KMER_LEN - 1] = vars[k];
 
@@ -403,14 +446,26 @@ void filter_sub_fstlst_cancer(int pid, int fid, kseq_t *seq, const char *sub,
 
                 uint32_t cnr = it->second.first;
                 uint32_t ctr = it->second.second;
-                char buf[256] = {0};
+
+                normal_counts[k] = cnr;
+                tumour_counts[k] = ctr;
+                normal_parent += cnr;
+                tumour_parent += ctr;
+            }
+
+            for (int k = 0; k < 4; k++) {
+                kmer[KMER_LEN - 1] = vars[k];
+
+                uint32_t cnr = normal_counts[k];
+                uint32_t ctr = tumour_counts[k];
+
                 if (ctr >= 4 && cnr == 0) {
+                    char buf[256] = {0};
                     sprintf(buf, "@%s\n%s\n+\n%s",
                             seq->name.s, seq->seq.s, seq->qual.s);
                     filter_tn_mutex.lock();
                     filter_tn_reads.insert(buf);
                     filter_tn_mutex.unlock();
-                    return;
                 }
             }
         }
