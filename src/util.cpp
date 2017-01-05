f6c3f324d698d9f4d20add9ebf24bbc238827e8e
Jordà Polo <jorda.polo@bsc.es>
2017-01-04 16:24:39 -0800

    Move non-specialized code to header

--- src/util.cpp
+++ src/util.cpp
@@ -1,38 +1,8 @@
 #include "util.hpp"
 
-#include <chrono>
-#include <fstream>
-#include <iostream>
-#include <thread>
-
-using std::cout;
-using std::endl;
-using std::string;
-
-// Generic and timed thread spawn, to avoid duplicating the same boilerplate
-// for different stages/steps.
-template<typename T>
-void spawn(string name, std::function<void(T)> func, std::vector<T> list)
-{
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    std::chrono::duration<double> time;
-    start = std::chrono::system_clock::now();
-
-    std::vector<std::thread> threads;
-    for (auto& i: list)
-        threads.push_back(std::thread(func, i));
-    cout << "Spawned " << threads.size() << " " << name << " threads" << endl;
-    for (auto& thread: threads)
-        thread.join();
-
-    end = std::chrono::system_clock::now();
-    time = end - start;
-    cout << "Spawn " << name << " time: " << time.count() << endl;
-}
-
 // Specialization to spawn N threads, passing a list of sequential integers:
 // [0..N).
-void spawn(string name, std::function<void(int)> func, int n)
+void spawn(std::string name, std::function<void(int)> func, int n)
 {
     std::vector<int> list;
     for (int i = 0; i < n; i++)
--- src/util.hpp
+++ src/util.hpp
@@ -1,11 +1,33 @@
 #ifndef __SM_UTIL_H__
 #define __SM_UTIL_H__
 
+#include <chrono>
+#include <fstream>
 #include <functional>
+#include <iostream>
+#include <thread>
 #include <vector>
 
 template<typename T>
-void spawn(std::string name, std::function<void(T)> func, std::vector<T> list);
+void spawn(std::string name, std::function<void(T)> func, std::vector<T> list)
+{
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double> time;
+    start = std::chrono::system_clock::now();
+
+    std::vector<std::thread> threads;
+    for (auto& i: list)
+        threads.push_back(std::thread(func, i));
+    std::cout << "Spawned " << threads.size() << " " << name << " threads"
+              << std::endl;
+    for (auto& thread: threads)
+        thread.join();
+
+    end = std::chrono::system_clock::now();
+    time = end - start;
+    std::cout << "Spawn " << name << " time: " << time.count() << std::endl;
+}
+
 void spawn(std::string name, std::function<void(int)> func, int n);
 
 float estimate_sparse(uint64_t n, size_t k, size_t v);
