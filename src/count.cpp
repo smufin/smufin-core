25dd1d6a84ec196d5350b93e0dd4b358822c12db
Jordà Polo <jorda.polo@bsc.es>
2017-01-25 13:33:26 +0100

    Display stems seen only once
    When running the prune stage without a cache, stems seen only once can provide
    an estimation of the false-positive rate of the bloom filters.

--- src/count.cpp
+++ src/count.cpp
@@ -355,11 +355,13 @@ void count::stats()
     std::map<uint64_t, uint64_t> hist_n, hist_t;
 
     uint64_t total_stems = 0;
+    uint64_t total_once = 0;
     uint64_t total_kmers = 0;
     uint64_t total_sum = 0;
 
     for (int i = 0; i < _conf.num_storers; i++) {
         uint64_t num_stems = _tables[i]->size();
+        uint64_t num_once = 0;
         uint64_t num_kmers = 0;
         uint64_t sum = 0;
         for (sm_table::const_iterator it = _tables[i]->begin();
@@ -379,11 +381,14 @@ void count::stats()
                 hist_n[int(log2(sum_n))]++;
             if (sum_t > 0)
                 hist_t[int(log2(sum_t))]++;
+            if (sum_n + sum_t == 1)
+                num_once++;
             sum += sum_n + sum_t;
         }
-        cout << "Table " << i << ": " << num_stems << " " << num_kmers
-             << " " << sum << endl;
+        cout << "Table " << i << ": " << num_stems << " " << num_once << " "
+             << num_kmers << " " << sum << endl;
         total_stems += num_stems;
+        total_once += num_once;
         total_kmers += num_kmers;
         total_sum += sum;
     }
@@ -399,6 +404,7 @@ void count::stats()
     }
 
     cout << "Number of stems: " << total_stems << endl;
+    cout << "Number of stems seen once: " << total_once << endl;
     cout << "Number of kmers: " << total_kmers << endl;
     cout << "Sum of counters: " << total_sum << endl;
 }
